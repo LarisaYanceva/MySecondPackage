cdc4a7df7d8f67136a0434ee92ef1371a1cf0542
Larisa Yanceva <larisa.yanceva@priorbank.by>
2019-05-01 11:29:05 +0300

    Add 2 functions: add and mult Also added some documentation

---
+++ R/MySecondPackage.R
@@ -0,0 +1,12 @@
+#' MySecondPackage: A package for computating the notorious bar statistic.
+#'
+#' The MySecondPackage package provides three categories of important functions:
+#' add and mult.
+#'
+#' @section MySecondPackage functions:
+#' The MySecondPackage functions ...
+#'
+#' @docType package
+#' @name MySecondPackage
+NULL
+#> NULL
---
+++ R/mult.R
@@ -0,0 +1,11 @@
+#' Multiply together two numbers.
+ #'
+   #' @param x A number.
+   #' @param y A number.
+   #' @return The product of \code{x} and \code{y}.
+   #' @examples
+   #' mult(1, 1)
+   #' mult(10, 1)
+   mult <- function(x, y) {
+         x * y
+    }
